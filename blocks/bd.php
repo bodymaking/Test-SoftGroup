70e5ad536224944bd1b1230140407cd00f259dbf
bodymaking <bodymaking@gmail.com>
2017-09-15 00:14:22 +0300

    TestCommit

---
+++ blocks/bd.php
@@ -0,0 +1,5 @@
+<?
+$db = mysql_connect ("allcgrou.mysql.ukraine.com.ua","allcgrou_buryak","wbg34lfv");
+mysql_select_db("allcgrou_buryak",$db);
+mysql_set_charset("utf8");
+?>
---
+++ blocks/footer.php
@@ -0,0 +1,5 @@
+<div id="templatemo_footer">
+
+		Copyright @ 2017 Buryakov Dmitriy
+
+    </div>
